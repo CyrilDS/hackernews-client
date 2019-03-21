a7b56c9dfec9d7a5d0cb06085f59768f721bdc46
Cyril <dasilva.cyril@gmail.com>
2019-03-21 15:25:11 +0100

    ES6 Spread Operators

--- src/App.js
+++ src/App.js
@@ -70,8 +70,10 @@ class App extends Component {
 
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
-    const updateList = this.state.list.filter(isNotId);
-    this.setState({ list: updateList });
+    const updatedHits = this.state.result.hits.filter(isNotId);
+    this.setState({
+      result: { ...this.state.result, hits: updatedHits }
+    });
   }
 
   onSearchChange(e) {
